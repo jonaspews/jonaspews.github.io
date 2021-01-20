bd0a938c7f13a1f79b0e3e9620844ec5794c75c7
Jonas Pews <jonas.pews@gmx.de>
2021-01-20 20:04:42 +0100

    new icon, own navbar-header with JS

--- script_q2_electrochemistry/js/scripts.js
+++ script_q2_electrochemistry/js/scripts.js
@@ -19,3 +19,22 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 } 
+
+
+
+// script to hide content active content panel in the header, 
+// when a chapter/topic is choosen
+ 
+var anchor = document.getElementsByClassName("hrefContent");
+var j;
+
+for (j = 0; j < anchor.length; j++) {
+  anchor[j].addEventListener("click", function() {
+        /* Toggle between hiding and showing the active panel */
+      var panel = document.getElementById("headerPanel");
+      panel.style.display = "none";
+    });
+}
+
+
+
